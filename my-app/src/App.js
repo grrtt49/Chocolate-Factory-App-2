8b2f295df52a4486cea193b8dba6a234f3a30df1
grrtt49 <awesomegarrett49@gmail.com>
2022-10-24 11:17:49 -0600

    App bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+    },
+    secondary: {
+      main: "#DEBE5A",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ResponsiveAppBar />
+    </ThemeProvider>
   );
 }
 
